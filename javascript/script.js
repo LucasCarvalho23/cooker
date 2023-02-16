0c8acf6a2f458acf40ab8539518b8b12ba6c71f1
Lucas Carvalho <lucasilva1990@gmail.com>
2023-02-16 19:42:39 -0300

    Oven

--- javascript/script.js
+++ javascript/script.js
@@ -2,20 +2,40 @@ class Cooker {
 
     constructor() {
         
+// Electricity
         this.buttonTurnOn = document.querySelector ("#buttonTurnOn-id")       
         this.spanTurnOn = document.querySelector ("#spanTurnOn-id")
-
         this.electricity = false
-
     
-        // Turn On and Turn Off the electricity
+// Turn On and Turn Off the electricity
         this.buttonTurnOn.addEventListener("click", () => { 
             this.turnOn()
         })
 
+
+//Oven
+        this.lighterOven = document.querySelector ("#lighterOven-id")
+        this.spanLighterOven = document.querySelector ("#spanLighterOven-id")
+        this.lighterOvenTurnOn = false
+
+// Turn On and Turn Off the oven
+        this.lighterOven.addEventListener("click", () => { 
+            if (this.lighterOvenTurnOn === false) {
+                this.turnOnOven()
+            } else {
+                this.turnOffOven()
+            }
+        })
+
     }
 
-    
+
+/* 
+    FUNCTIONS
+*/
+
+
+// Electricity
     turnOn() {
         if (this.electricity === false) {
             this.spanTurnOn.innerHTML = "Desligar"
@@ -27,6 +47,23 @@ class Cooker {
             this.electricity = false   
         }
     }
+
+
+// Oven    
+    turnOnOven() {
+        if (this.electricity === false) {
+            alert ("Ligue a eletricidade")
+        } else if (this.electricity === true) {
+            this.spanLighterOven.innerHTML = "Forno Ligado"
+            this.lighterOvenTurnOn = true 
+        } 
+    }
+
+    turnOffOven() {
+        this.spanLighterOven.innerHTML = "Forno Desligado"
+        this.lighterOvenTurnOn = false
+    }
+
 }
 
 
